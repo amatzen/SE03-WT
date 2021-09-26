ceea8cc50bd30cd433cbdbe8d4219351fb98e5c7
Alexander Matzen <alexander@amatzen.dk>
2021-09-26 19:16:29 +0200

    Self closing tag

--- Lesson3/Exercises/01_Familiarize_with_PHP/index.php
+++ Lesson3/Exercises/01_Familiarize_with_PHP/index.php
@@ -3,5 +3,5 @@ define('LENGTH_OF_PYRAMID', 7);
 
 for ($i = 1; $i < LENGTH_OF_PYRAMID+1; $i++) {
     $arr = array_fill(0, $i, "#");
-    echo join("", $arr) . "<br>";
+    echo join("", $arr) . "<br />";
 }
